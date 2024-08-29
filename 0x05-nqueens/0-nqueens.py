cdf276679572164bee5c6c0dc443d72d1707962d
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-29 15:23:58 +0300

    task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,60 +1,44 @@
 #!/usr/bin/python3
-
+""" N queens """
 import sys
 
-def is_safe(board, row, col):
-    """Check if it's safe to place a queen at board[row][col]."""
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    for i, j in zip(range(row, len(board), 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-    return True
 
-def solve_nqueens_util(board, col, results):
-    """Utilize backtracking to find all solutions."""
-    if col >= len(board):
-        results.append([[i, board[i].index(1)] for i in range(len(board))])
-        return True
-    res = False
-    for i in range(len(board)):
-        if is_safe(board, i, col):
-            board[i][col] = 1
-            res = solve_nqueens_util(board, col + 1, results) or res
-            board[i][col] = 0
-    return res
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-def solve_nqueens(n):
-    """Main function to solve the N Queens problem."""
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    results = []
-    solve_nqueens_util(board, 0, results)
-    for solution in results:
-        print(solution)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-def validate_and_parse_args():
-    """Validate and parse command line arguments."""
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    
-    return n
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-if __name__ == "__main__":
-    n = validate_and_parse_args()
-    solve_nqueens(n)
+n = int(sys.argv[1])
 
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
